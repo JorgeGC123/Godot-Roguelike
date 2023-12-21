6250d27e67f7199602ebc2c5cb212908ec6fab54
Jorge Alvaro Garzón <kalwhur@gmail.com>
2023-12-21 16:09:47 +0100

    added: tooltips on breakables

--- Characters/Breakables/Breakable.gd
+++ Characters/Breakables/Breakable.gd
@@ -1,15 +1,30 @@
 extends Character
 
-var held_breakable: Node = null  # Referencia al objeto Breakable que está sosteniendo
+var held_breakable: Node = null
 onready var tween: Tween = Tween.new()
+onready var tooltip: Label = $Tooltip  # Asegúrate de tener un nodo Label como hijo para el tooltip
+var initial_tooltip_position: Vector2
 
 func _ready():
 	add_child(tween)
-	
+	tooltip.visible = false  # El tooltip se inicia invisible
+	var font = DynamicFont.new()
+	font.font_data = load("res://Fonts/Rubik-DoodleShadow-Regular.ttf")
+	font.size = 12
+	tooltip.add_font_override("font", font)
+
 func _on_Area2D_body_entered(body):
 	if body is Player:
-		body.near_breakable = self  # Establece que el jugador está cerca de un Breakable
+		body.near_breakable = self
+		update_tooltip_position()
+		tooltip.text = str("e")  # Muestra la tecla de interacción
+		tooltip.visible = true
 
 func _on_Area2D_body_exited(body):
 	if body is Player:
-		body.near_breakable = null  # Establece que el jugador ya no está cerca de un Breakable
+		body.near_breakable = null
+		tooltip.visible = false
+
+func update_tooltip_position():
+	tooltip.rect_position = initial_tooltip_position + Vector2(-5, -20)
+		
